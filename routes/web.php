eac898e14257d7effcb146bb75640e3d421656c9
JotaVe <nicolas.steck@hotmail.com>
2019-06-04 18:18:54 -0300

    rotas de get e post criadas

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/sobre', 'PagesController@sobre');
-
-Route::get('/amigos', 'PagesController@amigos');
+Route::get('/cadastro', 'CowController@cadastroVacas');
+Route::get('/lista', 'CowController@listaVacas');
+Route::post('/formCadastro', 'CowController@cadastraVacas');
